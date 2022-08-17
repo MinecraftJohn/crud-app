128cfccba3acc702de6b3977d2c29fa145c33106
John Napoles <84890923+MinecraftJohn@users.noreply.github.com>
2022-08-17 22:14:36 +0800

    backup files for incoming revamp

--- crudhub/index.php
+++ crudhub/index.php
@@ -61,81 +61,86 @@
                     <p class="table_row_actions">Actions</p>
                 </div>
                 <div class="table_content">
+                    <p class="table_empty_message">Sorry, there is no data available.</p>
                     <?php 
                         $selectName = "SELECT * FROM users";
                         $selectNameResult = mysqli_query($mysqlConnect, $selectName);
                         $selectNameFetch = mysqli_fetch_assoc($selectNameResult);
 
-                        for ($i=1; $i <= $selectNameResult->num_rows; $i++) { 
-                            $selectData = "SELECT * FROM users WHERE id=$i;";
-                            $selectDataResult = mysqli_query($mysqlConnect, $selectData);
-                            $selectDataFetch = mysqli_fetch_assoc($selectDataResult);
-                            echo "<div class='table_data'>
-                                    <p class='table_row_name'>"
-                                        .$selectDataFetch['fname']." ". $selectDataFetch['lname'].
-                                    "</p>
-                                    <p class='table_row_birthdate'>";
-                                        switch ($selectDataFetch['bdmonth']) {
-                                            case 1:
-                                                echo 'Jan';
-                                                break;
-                                            case 2:
-                                                echo 'Feb';
-                                                break;
-                                            case 3:
-                                                echo 'Mar';
-                                                break;
-                                            case 4:
-                                                echo 'Apr';
-                                                break;
-                                            case 5:
-                                                echo 'May';
-                                                break;
-                                            case 6:
-                                                echo 'Jun';
-                                                break;
-                                            case 7:
-                                                echo 'Jul';
-                                                break;
-                                            case 8:
-                                                echo 'Aug';
-                                                break;
-                                            case 9:
-                                                echo 'Sep';
-                                                break;
-                                            case 10:
-                                                echo 'Oct';
-                                                break;
-                                            case 11:
-                                                echo 'Nov';
-                                                break;
-                                            case 12:
-                                                echo 'Dec';
-                                                break;
-                                            
-                                            default:
-                                                echo 'Err';
-                                                break;
-                                        }
-                                    echo " ".$selectDataFetch['bdday'].", ".$selectDataFetch['bdyear'].
-                                    "</p>
-                                    <p class='table_row_email'>"
-                                        .$selectDataFetch['email'].
-                                    "</p>
-                                    <p class='table_row_phone'>"
-                                        .$selectDataFetch['phone'].
-                                    "</p>
-                                    <div class='table_row_actions'>
-                                        <svg class='table_row_actions_icon' viewBox='0 0 24 24'>
-                                            <title>Edit</title>
-                                            <path d='M19.769 9.923l-12.642 12.639-7.127 1.438 1.438-7.128 12.641-12.64 5.69 5.691zm1.414-1.414l2.817-2.82-5.691-5.689-2.816 2.817 5.69 5.692z' fill='#1a73e8'/>
-                                        </svg>
-                                        <svg class='table_row_actions_icon' viewBox='0 0 24 24'>
-                                            <title>Delete</title>
-                                            <path d='M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2m-9-14c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6 0c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6-5h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-12-2h4v-1h-4v1z' fill='#e81123'/>
-                                        </svg>
-                                    </div>
-                                </div>";
+                        if (0 == $selectNameResult->num_rows) {
+                            echo "<script>document.getElementsByClassName('table_empty_message')[0].style.display = 'flex'</script>";
+                        } else {
+                            for ($i=1; $i <= $selectNameResult->num_rows; $i++) { 
+                                $selectData = "SELECT * FROM users WHERE id=$i;";
+                                $selectDataResult = mysqli_query($mysqlConnect, $selectData);
+                                $selectDataFetch = mysqli_fetch_assoc($selectDataResult);
+                                echo "<div class='table_data'>
+                                        <p class='table_row_name'>"
+                                            .$selectDataFetch['fname']." ". $selectDataFetch['lname'].
+                                        "</p>
+                                        <p class='table_row_birthdate'>";
+                                            switch ($selectDataFetch['bdmonth']) {
+                                                case 1:
+                                                    echo 'Jan';
+                                                    break;
+                                                case 2:
+                                                    echo 'Feb';
+                                                    break;
+                                                case 3:
+                                                    echo 'Mar';
+                                                    break;
+                                                case 4:
+                                                    echo 'Apr';
+                                                    break;
+                                                case 5:
+                                                    echo 'May';
+                                                    break;
+                                                case 6:
+                                                    echo 'Jun';
+                                                    break;
+                                                case 7:
+                                                    echo 'Jul';
+                                                    break;
+                                                case 8:
+                                                    echo 'Aug';
+                                                    break;
+                                                case 9:
+                                                    echo 'Sep';
+                                                    break;
+                                                case 10:
+                                                    echo 'Oct';
+                                                    break;
+                                                case 11:
+                                                    echo 'Nov';
+                                                    break;
+                                                case 12:
+                                                    echo 'Dec';
+                                                    break;
+                                                
+                                                default:
+                                                    echo 'Err';
+                                                    break;
+                                            }
+                                        echo " ".$selectDataFetch['bdday'].", ".$selectDataFetch['bdyear'].
+                                        "</p>
+                                        <p class='table_row_email'>"
+                                            .$selectDataFetch['email'].
+                                        "</p>
+                                        <p class='table_row_phone'>"
+                                            .$selectDataFetch['phone'].
+                                        "</p>
+                                        <div class='table_row_actions'>
+                                            <svg class='table_row_actions_icon' viewBox='0 0 24 24'>
+                                                <title>Edit</title>
+                                                <path d='M19.769 9.923l-12.642 12.639-7.127 1.438 1.438-7.128 12.641-12.64 5.69 5.691zm1.414-1.414l2.817-2.82-5.691-5.689-2.816 2.817 5.69 5.692z' fill='#1a73e8'/>
+                                            </svg>
+                                            <svg class='table_row_actions_icon' viewBox='0 0 24 24'>
+                                                <title>Delete</title>
+                                                <path d='M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2m-9-14c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6 0c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6-5h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-12-2h4v-1h-4v1z' fill='#e81123'/>
+                                            </svg>
+                                        </div>
+                                    </div>";
+                            }
                         }
                     ?>
                 </div>
