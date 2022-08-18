c729908fd4cd34d0c9a57546b42ec7cc69c91556
John Napoles <84890923+MinecraftJohn@users.noreply.github.com>
2022-08-18 08:40:10 +0800

    Delete _connect.php

--- crudhub/_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $dbHostName = 'localhost';
-    $dbUsername = 'root';
-    $dbPassword = '';
-    $dbName = 'crudhub';
-
-    $mysqlConnect = mysqli_connect("$dbHostName", "$dbUsername", "$dbPassword", "$dbName");
-?>
