ef263e14268a1be06162c3c1166922248b14970f
John Napoles <84890923+MinecraftJohn@users.noreply.github.com>
2022-08-18 08:40:42 +0800

    Delete variables.js

--- crudhub/assets/js/variables.js
+++
@@ -1,4 +0,0 @@
-// NAVIGATION VARIABLES
-var navMobileBackground = document.getElementsByClassName("nav_mobile_background");
-var navIcon = document.getElementsByClassName("nav_icon");
-var navMobileContainer = document.getElementsByClassName("nav_mobile_container");
